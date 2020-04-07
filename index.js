d8fb0524fb9c2f7eb80f31eb40903019cc871220
Diana Stebbins <dianastebbins97@gmail.com>
2020-04-07 12:07:10 -0700

    we can add a reservationgit add .!

--- index.js
+++ index.js
@@ -31,13 +31,6 @@ const waitListArr = [
     }
 ];
 
-// reservation
-// name, phone, email, id
-
-// ROUTES 
-// get table data
-// set table data
-
 // display home
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
@@ -52,6 +45,22 @@ app.get("/tables", function (req, res) {
 app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
+
+app.post("/api/reserved", function(req, res){
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newReservation = req.body;
+  
+    // // Using a RegEx Pattern to remove spaces from newCharacter
+    // // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    // newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newReservation);
+  
+    reservationArr.push(newReservation);
+  
+    res.json(newReservation);
+  });
 
 // display views: waitlist
 app.get("/api/wait", function (req, res) {
